fab130ea1b6b5324e295a3c7a03c45dfc61ee5b1
ageery <andrew_geery@windsolutions.com>
2019-10-15 07:53:37 -0400

    Added jooq task to the example and updated documentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2019 Andrew Geery
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import dev.bombinating.gradle.jooq.*
-import org.jooq.meta.jaxb.Logging
+import org.flywaydb.gradle.task.FlywayMigrateTask
 
 val genDir = "$projectDir/generated/src/main/java"
+val genDir2 = "$projectDir/generated2/src/main/java"
 
 val h2Version: String by project
 val jooqVersion: String by project
@@ -10,6 +26,10 @@ val dbUrl: String by project
 val dbUsername: String by project
 val dbPassword: String by project
 
+val dbUrl2: String by project
+val dbUsername2: String by project
+val dbPassword2: String by project
+
 plugins {
     java
     id("dev.bombinating.jooq-codegen")
@@ -17,7 +37,7 @@ plugins {
 }
 
 sourceSets["main"].java {
-    srcDir(genDir)
+    srcDirs(genDir, genDir2)
 }
 
 repositories {
@@ -37,9 +57,15 @@ flyway {
     password = dbPassword
 }
 
+val flywayMigrate2 = tasks.register<FlywayMigrateTask>("flywayMigrate2") {
+    url = dbUrl2
+    user = dbUsername2
+    password = dbPassword2
+    locations = arrayOf("filesystem:$projectDir/src/main/resources/db2/migration")
+}
+
 jooq {
     version = jooqVersion
-    logging = Logging.DEBUG
     jdbc {
         url = dbUrl
         username = dbUsername
@@ -48,6 +74,7 @@ jooq {
     generator {
         database {
             inputSchema = "PUBLIC"
+            excludes = "FlywaySchemaHistory"
         }
         target {
             directory = genDir
@@ -56,11 +83,30 @@ jooq {
     }
 }
 
+val jooq2 = tasks.register<JooqTask>("jooq2") {
+    jdbc {
+        url = dbUrl2
+        username = dbUsername2
+        password = dbPassword2
+    }
+    generator {
+        database {
+            inputSchema = "PUBLIC"
+            excludes = "FlywaySchemaHistory"
+        }
+        target {
+            directory = genDir2
+            packageName = "dev.bombinating.db2"
+        }
+    }
+    dependsOn(flywayMigrate2)
+}
+
 tasks.register("cleanGenerated") {
     doLast {
-        delete(genDir)
+        delete(genDir, genDir2)
     }
 }
 
 tasks.getByName("jooq").dependsOn(tasks.getByName("flywayMigrate"))
-tasks.getByName("compileJava").dependsOn(tasks.getByName("jooq"))
+tasks.getByName("compileJava").dependsOn(tasks.getByName("jooq"), jooq2)
---
+++ src/main/resources/db2/migration/V1__Company.sql
@@ -0,0 +1,4 @@
+create table company (
+    id int,
+    company_name varchar(100)
+);
